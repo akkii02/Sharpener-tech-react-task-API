230951b117d5ac7de4497c14c8c9a37f4c808346
akshaysable02 <akshaysable097@gmail.com>
2023-11-25 15:23:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+  const [movies, setMovies] = useState([]);
+
+  async function fetchMoviesHandler() {
+    try {
+      const response = await fetch('https://swapi.dev/api/films/');
+      const data = await response.json();
+
+      const transformedMovies = data.results.map(moviesData => ({
+        id: moviesData.episode_id,
+        title: moviesData.title,
+        openingText: moviesData.opening_crawl,
+        releaseDate: moviesData.release_date
+      }));
+
+      setMovies(transformedMovies);
+    } catch (error) {
+      console.error('Error fetching movies:', error);
+    }
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
