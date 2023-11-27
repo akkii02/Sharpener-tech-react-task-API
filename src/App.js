d46c155db6c60b57e82f8e44899dddfabb714856
akshaysable02 <akshaysable097@gmail.com>
2023-11-27 22:11:22 +0530

    Adding new Movies intio the list

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import MoviesList from './components/MoviesList';
-import './App.css';
+import React, { useState, useEffect, useCallback } from "react";
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+import AddMovies from "./components/AddMovies";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
- 
-
   const fetchMovies = useCallback(async () => {
     setIsLoading(true);
     setError(null);
 
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch("https://swapi.dev/api/films/");
       if (!response.ok) {
-        throw new Error('Something went wrong. Please try again later.');
+        throw new Error("Something went wrong. Please try again later.");
       }
       const data = await response.json();
 
@@ -33,7 +32,7 @@ function App() {
     } finally {
       setIsLoading(false);
     }
-  })
+  });
 
   useEffect(() => {
     fetchMovies();
@@ -65,9 +64,14 @@ function App() {
   if (isLoading) {
     content = <p>Loading....</p>;
   }
-
+  const addMovieHandler = (movie) => {
+    console.log(movie);
+  };
   return (
     <React.Fragment>
+      <section>
+        <AddMovies onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMovies} disabled={isLoading}>
           Fetch Movies
---
+++ src/components/AddMovies.js
@@ -0,0 +1,37 @@
+import React, { useRef } from "react";
+import classes from "./AddMovies.module.css";
+const AddMovies = (props) => {
+  const titleRef = useRef("");
+  const openingTextRef = useRef("");
+  const releaseDateRef = useRef("");
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const moviesList = {
+      title: titleRef.current.value,
+      description: openingTextRef.current.value,
+      releaseDate: releaseDateRef.current.value,
+    };
+    props.onAddMovie(moviesList);
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <div className={classes.control}>
+        <label htmlFor="title">Title</label>
+        <input type="text" id="title" ref={titleRef} />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="opening-text">Opening Text</label>
+        <textarea rows="5" id="opening-text" ref={openingTextRef}></textarea>
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="date" ref={releaseDateRef}>
+          Release Date
+        </label>
+        <input type="text" id="date" />
+      </div>
+      <button className={classes.btn}>Add Movie</button>
+    </form>
+  );
+};
+
+export default AddMovies;
